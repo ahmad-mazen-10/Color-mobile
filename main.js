ad6c45570eb1f4f82518b03e75d4783c7774b1a2
Ahmed Mazen <ahmadmazen060@gmail.com>
2023-10-05 18:41:56 +0300

    Created project color back mobile

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.pic1');
+let container = document.querySelector('.container');
+
+function phones(pic){
+    img.src = pic;
+}
+function colors(color){
+    container.style.background = color;
+}
